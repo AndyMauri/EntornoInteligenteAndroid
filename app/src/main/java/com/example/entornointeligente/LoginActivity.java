02cb56924b2cc6c00e2e934bd9ade8e32efb797f
el-va <esanchez4257@misena.edu.co>
2019-05-31 22:45:52 -0500

    ADSI-168

--- app/src/main/java/com/example/entornointeligente/LoginActivity.java
+++ app/src/main/java/com/example/entornointeligente/LoginActivity.java
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -30,6 +31,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -63,7 +65,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             };
         }
     };*/
-    public static String Ip="192.168.0.5:800", E, C;
+    public static String Ip = "10.5.20.200", E, C, nombre, idRol;
+
 
     private static final ThreadLocal<String[]> DUMMY_CREDENTIALS = new ThreadLocal<String[]>() {
         @Override
@@ -75,19 +78,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     };
 
 
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private UserLoginTask mAuthTask = null;
-
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
-    String response, nombre;
-
-
+    String response;
+    MediaPlayer bienvenido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,6 +92,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         setContentView(R.layout.activity_login);
         // Set up the login form.
         mEmailView = findViewById(R.id.email);
+        bienvenido = MediaPlayer.create(this, R.raw.bienvenido);
         populateAutoComplete();
 
         mPasswordView = findViewById(R.id.password);
@@ -114,36 +112,52 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             @Override
             public void onClick(View view) {
 
-
+                if (attemptLogin() == false) {
+                    return;
+                }
 
                 new Thread(new Runnable() {
 
                     @Override
                     public void run() {
 
-                        response = HttpRequest.get("http://" + Ip + "/Servicio_Proyect/Servicio_usuario/Select_Login.php?usuario=" + mEmailView.getText().toString()
-                                + "&contrasena=" + mPasswordView.getText().toString()).body();
+
+                        response = HttpRequest.get("http://" + Ip + "/Servicio_Proyect/Servicio_usuario/Select_Login.php?usuario=" + mEmailView.getText().toString() + "&contrasena=" + mPasswordView.getText().toString()).body();
                         System.out.println("Response was: " + response);
 
                         try {
                             final JSONArray obj = new JSONArray(response);
 
-                            runOnUiThread(new Runnable() {
+                            if (obj.length() == 1) {
+                                runOnUiThread(new Runnable() {
 
-                                @Override
-                                public void run() {
-                                    try {
-                                        E= obj.getJSONArray(0).getString(0);
-                                        C= obj.getJSONArray(0).getString(1);
-                                        nombre=obj.getJSONArray(0).getString(2);
-                                        System.out.println("Response was: " + E+" ... "+C);
-                                        attemptLogin();
-                                    } catch (JSONException e) {
-                                        // TODO Auto-generated catch block
-                                        e.printStackTrace();
+                                    @Override
+                                    public void run() {
+                                        try {
+                                            E = obj.getJSONArray(0).getString(0);
+                                            C = obj.getJSONArray(0).getString(1);
+                                            nombre = obj.getJSONArray(0).getString(2);
+                                            idRol = obj.getJSONArray(0).getString(3);
+                                            System.out.println("Response was: " + E + " ... " + C);
+                                            Intent trans = new Intent(getApplicationContext(), Principal.class);
+                                            bienvenido.start();
+                                            startActivity(trans);
+                                        } catch (JSONException e) {
+                                            // TODO Auto-generated catch block
+                                            e.printStackTrace();
+                                        }
                                     }
-                                }
-                            });
+                                });
+
+                            } else {
+                                runOnUiThread(new Runnable() {
+
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(getApplicationContext(), "Usuario no existe", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }
 
 
                         } catch (JSONException e) {
@@ -154,7 +168,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                     }
                 }).start();
 
-                if (response==null ||  mEmailView==null || mPasswordView==null) {
+                if (response == null || mEmailView == null || mPasswordView == null) {
                     attemptLogin();
                 }
 
@@ -195,6 +209,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                         @TargetApi(Build.VERSION_CODES.M)
                         public void onClick(View v) {
                             requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+
                         }
                     });
         } else {
@@ -223,11 +238,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * errors are presented and no actual login attempt is made.
      */
 
-    private void attemptLogin() {
-
-        if (mAuthTask != null) {
-            return;
-        }
+    private boolean attemptLogin() {
+        boolean puedeSeguir = true;
 
         // Reset errors.
         mEmailView.setError(null);
@@ -241,46 +253,44 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+        if (TextUtils.isEmpty(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
+            puedeSeguir = false;
         }
 
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
+            mEmailView.setError("El correo es vacio");
             focusView = mEmailView;
             cancel = true;
+            puedeSeguir = false;
         } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
+            mEmailView.setError("El correo es invalido");
             focusView = mEmailView;
             cancel = true;
+            puedeSeguir = false;
         }
 
         if (cancel) {
             // There was an error; don't attempt login and focus the first
             // form field with an error.
             focusView.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
         }
+        return puedeSeguir;
     }
 
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@") && email.contains(".") && email.equalsIgnoreCase(E);
+        return email.contains("@") && email.contains(".");
 
     }
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-        return password.length() >= 4 && password.equalsIgnoreCase(C);
+        return password.length() >= 4;
     }
 
     /*private boolean isEmailValid(String email) {
@@ -386,67 +396,4 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         int IS_PRIMARY = 1;
     }
 
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS.get()) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail) && pieces[1].equals(mPassword)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                finish();
-
-                Intent ob = new Intent(LoginActivity.this, Principal.class);
-                ob.putExtra("nombre",nombre);
-                ob.putExtra("correo", C);
-                startActivity(ob);
-
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
-    }
 }
